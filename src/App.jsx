3a884cceaa23d4a0de3d2b6767051f3f8d031742
gk <gkpattanaik001@gmail.com>
2025-04-26 23:30:43 +0530

    deleted unnecessary files

--- src/App.jsx
+++ src/App.jsx
@@ -31,44 +31,7 @@ const App = () => {
     <Router>
       <Routes>
         {/* Landing Page */}
-        <Route
-          path="/"
-          element={
-            <div className="bg-white w-full overflow-hidden">
-              <div className="flex flex-col md:flex-row justify-center md:justify-start items-center gap-4 px-4 py-2 mx-12 bg-blue-50 text-sm">
-                <div className="flex items-center gap-2 text-blue-700 font-medium">
-                  <FaPhoneAlt className="text-blue-600" />
-                  <span>7752058263</span>
-                </div>
-                <div className="flex items-center gap-2 text-blue-700 font-medium">
-                  <FaEnvelope className="text-blue-600" />
-                  <span>gkpattanaik001@gmail.com</span>
-                </div>
-              </div>
-
-              <Navbar />
-
-              <div className={`bg-white`}>
-                <Hero />
-              </div>
-
-              <div className={`bg-white`}>
-                <AboutStats />
-                <StudentPath />
-                {/* <Journey /> */}
-                <Business />
-
-                <TopColleges />
-                <Scholarship />
-                
-                  <div className="inline-block bg-[#009933] text-white text-sm font-semibold ml-8 px-4 py-1 rounded-full mb-4 animate-bounce">
-        Student and alumni connect
-                </div>
-                <CTA />
-                <Footer />
-              </div>
-            </div>
-          }
+        <Route     
         />
 
         {/* Scholarship Page */}
